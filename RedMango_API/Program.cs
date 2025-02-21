4ba4d3b192e105ee30caff370061dae54ef832a9
Farid Dibirov <farid962@gmail.com>
2025-02-21 17:33:13 +0400

    setup connectionstring and dbcontext, create menuitems, seed menu items, get menu item and api response, add storage account constring, implement blobservice

--- RedMango_API/Program.cs
+++ RedMango_API/Program.cs
@@ -1,6 +1,23 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RedMango_API.Data;
+using RedMango_API.Models;
+using RedMango_API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDbConnection"));
+});
+
+builder.Services.AddSingleton(u => new BlobServiceClient(builder.Configuration.GetConnectionString("StorageAccount")));
+
+builder.Services.AddSingleton<IBlobService, BlobService>();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
